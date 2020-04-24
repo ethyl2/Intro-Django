000975495ddd02d21eeb2c5910be62cac7f30322
Heather Nuffer <zzzuzz@gmail.com>
2020-04-24 15:06:57 -0600

    Adds api.py in notes folder

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,3 +9,9 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
